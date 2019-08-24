672e7473403d7b56c332889369ae9769b2c0d621
Gabriel <espiritosanto.gabriel@gmail.com>
2019-08-24 10:57:38 -0300

    grafo feito

--- main.cpp
+++ main.cpp
@@ -4,38 +4,6 @@
 #include "dolphin.h"
 
 using namespace std;
-//  Essa funcao adiciona a indexacao base na lista total
-/*void verificaMinMax() {
-    FILE *dolphins;
-    int refId, id
-    int minId = 999, maxId = 0;
-    int minRefId = 999, maxRefId = 0;
-    Dolphin dolphin, refDolphin;
-
-    Graph Grafo;
-    //list <Dolphin> listaTotal;
-
-    char file[] = "dolphins.txt";
-    dolphins = fopen(file, "r");
-
-    if (dolphins == NULL) {
-        cout << "Erro ao abrir Arquivo" << endl;
-    }
-    while (fscanf(dolphins, "%d %d", &id, &refId) != EOF) {
-        //Seta o Id de cada elemento do Grafo
-        if (id < minId){
-            minId = id;
-        }
-        if (refId < minRefId){
-            minRefId = refId;
-        }
-    }
-    if (minId > minRefId)
-        minId = minRefId;
-    cout << minId << endl;
-    fclose(dolphins);
-
-}*/
 
 void leArquivo() {
     FILE *dolphins;
@@ -94,8 +62,6 @@ void leArquivo() {
     fclose(dolphins);
     int total = 0;
     for (auto & i : Grafo.listaTotal ) {
-        //cout << i.getId() << endl;
-        //cout << Grafo.listaTotal.size() << endl;
         cout << "Base: "<< i.getId() << " " << endl;
         cout << "Tamanho de conexoes: " << i.conexoes.size() << endl;
         for (auto & j : i.conexoes )
@@ -107,36 +73,6 @@ void leArquivo() {
     cout << "TOTAL DE CONEXOES: "<< total << endl;
 
 }
-
-/*void leArquivo() {
-    FILE *dolphins;
-    int refId, id;
-    Dolphin dolphin, refDolphin;
-
-    list <Dolphin> listaTotal;
-
-    char file[] = "dolphins.txt";
-    dolphins = fopen(file, "r");
-
-    if (dolphins == NULL) {
-        cout << "Erro ao abrir Arquivo" << endl;
-    }
-    while (fscanf(dolphins, "%d %d", &id, &refId) != EOF) {
-        //Seta o Id de cada elemento do Grafo
-        dolphin.setId(id);
-
-        //Manda o elemento do Grafo pra Lista
-        listaTotal.push_back(dolphin);
-        
-        refDolphin.setId(refId);
-
-        dolphin.setConnection(refDolphin);
-
-        cout << dolphin.getId() << " " << refDolphin.getId() << endl;
-    }
-    fclose(dolphins);
-
-}*/
 
 int main() {
         leArquivo();
