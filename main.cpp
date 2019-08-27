dda076c3d6daf7a6146ffc11fb1115fd752d5c26
Gabriel Oliveira do Espírito Santo <salanho@MacBook.local>
2019-08-27 19:06:07 -0300

    agora ve os dois lados

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include <list> 
+#include <list>
 #include <iterator>
 #include "dolphin.h"
 
 using namespace std;
 
-void leArquivo() {
+void leArquivo()
+{
     FILE *dolphins;
     int refId, id;
     int minId = 999, maxId = 0;
@@ -15,29 +16,34 @@ void leArquivo() {
 
     Graph Grafo;
 
-    for (int i = 0; i < 62; i++){
-        for (int j = 0; j < 62; j++){
+    for (int i = 0; i < 62; i++)
+    {
+        for (int j = 0; j < 62; j++)
+        {
             Grafo.map[i][j] = 0;
         }
         cout << endl;
-    } 
+    }
     //list <Dolphin> listaTotal;
 
     char file[] = "dolphins.txt";
     dolphins = fopen(file, "r");
 
-    if (dolphins == NULL) {
+    if (dolphins == NULL)
+    {
         cout << "Erro ao abrir Arquivo" << endl;
     }
-    while (fscanf(dolphins, "%d %d", &id, &refId) != EOF) {
+    while (fscanf(dolphins, "%d %d", &id, &refId) != EOF)
+    {
         //Seta o Id de cada elemento do Grafo
         Dolphin dolphin, refDolphin;
 
         naoEncontrado = false;
-        dolphin.setId(id);        
+        dolphin.setId(id);
         refDolphin.setId(refId);
-        
-        if (Grafo.listaTotal.size() == 0) {
+
+        if (Grafo.listaTotal.size() == 0)
+        {
             //listaTotal.push_back(dolphin);
             dolphin.setConnection(refDolphin);
             Grafo.listaTotal.push_back(dolphin);
@@ -45,44 +51,102 @@ void leArquivo() {
             //cout << "ELEMENTO ZERO: " << endl;
             //cout << dolphin.getId() << " " << refDolphin.getId() << endl;
         }
-        else {
-            for (auto & i : Grafo.listaTotal ) {
+        else
+        {
+            for (auto &i : Grafo.listaTotal)
+            {
                 //cout << i.getId() << endl;
-                if (id == i.getId()){
+                if (id == i.getId())
+                {
                     i.setConnection(refDolphin);
                     Grafo.map[id][refId] = 1;
                     //cout << "ELEMENTO JA EXISTE: " << endl;
                     //cout << i.getId() << " " << refDolphin.getId() << endl;
                     naoEncontrado = true;
                 }
-                
             }
-            if (!naoEncontrado) {
+            if (!naoEncontrado)
+            {
                 //cout << "ELEMENTO NAO EXISTE: " << endl;
                 //listaTotal.push_back(dolphin);
                 dolphin.setConnection(refDolphin);
                 Grafo.listaTotal.push_back(dolphin);
                 Grafo.map[id][refId] = 1;
             }
-                //cout << dolphin.getId() << " " << refDolphin.getId() << endl;
-
+            //cout << dolphin.getId() << " " << refDolphin.getId() << endl;
         }
     }
-    
+
     fclose(dolphins);
+
+    //Le lado esquerdo agr
+    dolphins = fopen(file, "r");
+
+    if (dolphins == NULL)
+    {
+        cout << "Erro ao abrir Arquivo" << endl;
+    }
+    while (fscanf(dolphins, "%d %d", &refId, &id) != EOF)
+    {
+        //Seta o Id de cada elemento do Grafo
+        Dolphin dolphin, refDolphin;
+
+        naoEncontrado = false;
+        dolphin.setId(id);
+        refDolphin.setId(refId);
+
+        if (Grafo.listaTotal.size() == 0)
+        {
+            //listaTotal.push_back(dolphin);
+            dolphin.setConnection(refDolphin);
+            Grafo.listaTotal.push_back(dolphin);
+            Grafo.map[id][refId] = 1;
+            //cout << "ELEMENTO ZERO: " << endl;
+            //cout << dolphin.getId() << " " << refDolphin.getId() << endl;
+        }
+        else
+        {
+            for (auto &i : Grafo.listaTotal)
+            {
+                //cout << i.getId() << endl;
+                if (id == i.getId())
+                {
+                    i.setConnection(refDolphin);
+                    Grafo.map[id][refId] = 1;
+                    //cout << "ELEMENTO JA EXISTE: " << endl;
+                    //cout << i.getId() << " " << refDolphin.getId() << endl;
+                    naoEncontrado = true;
+                }
+            }
+            if (!naoEncontrado)
+            {
+                //cout << "ELEMENTO NAO EXISTE: " << endl;
+                //listaTotal.push_back(dolphin);
+                dolphin.setConnection(refDolphin);
+                Grafo.listaTotal.push_back(dolphin);
+                Grafo.map[id][refId] = 1;
+            }
+            //cout << dolphin.getId() << " " << refDolphin.getId() << endl;
+        }
+    }
+
+    fclose(dolphins);
+
     int total = 0;
-    for (auto & i : Grafo.listaTotal ) {
-        cout << "Base: "<< i.getId() << " " << endl;
+    for (auto &i : Grafo.listaTotal)
+    {
+        cout << "Base: " << i.getId() << " " << endl;
         cout << "Tamanho de conexoes: " << i.conexoes.size() << endl;
-        for (auto & j : i.conexoes )
+        for (auto &j : i.conexoes)
             cout << j.getId() << " ";
-        cout << endl << endl;
+        cout << endl
+             << endl;
         total += i.conexoes.size();
     }
     cout << "TOTAL DE ELEMENTOS: " << Grafo.listaTotal.size() << endl;
-    cout << "TOTAL DE CONEXOES: "<< total << endl;
+    cout << "TOTAL DE CONEXOES: " << total << endl;
 
-    for (int i = 0; i < 62; i++){
+    /* for (int i = 0; i < 62; i++){
         cout << i+1 << "\t";
 
         for (int j = 0; j < 62; j++){
@@ -96,13 +160,13 @@ void leArquivo() {
             }
         }
         cout << endl;
-    } 
-
+    }  */
 }
 
-int main() {
-        leArquivo();
-        //verificaMinMax();
-        //cout << "Hello, World!" << endl;
+int main()
+{
+    leArquivo();
+    //verificaMinMax();
+    //cout << "Hello, World!" << endl;
     return 0;
 }
