589de55e3e0546a039fb9dda22c3225af30add47
andreyresende <38325819+andreyresende@users.noreply.github.com>
2019-09-01 00:14:11 +0000

    Grau e coeficientes prontos

--- main.cpp
+++ main.cpp
@@ -33,28 +33,56 @@ void Grafo::adicionar_relacao(int golfinho1, int golfinho2){
 
 void Grafo::mostrar_graus(){
     int i=1;
+    int *ptr;
     while(i <= 62){
         cout <<"O golfinho nº " << i << " possui " << vizinhos[i].size() << " amigo(s)" << endl;
         i++;
     }
-    cout << "Teste " << vizinhos[1].() << endl;
 }
 
 void Grafo::mostrar_maximais(){//Implementado utilizando o algoritmo de Bron-Kerbosch
     list<int> R, P, X;
-    int iterator;
     for(int i=1; i<=62; i++){
         P.push_back(i);
     }
-    if(P.empty() && X.empty(){
+    if(P.size() == 0 && X.size() == 0){
         cout << "Clique de tamanho " << R.size() << "  - Vertices: ";
-        iterator = R.begin();
-        while(iterator != NULL){
-            cout << iterator << " ";
-            iterator++;
-        }
         cout << endl;
     }
+}
+
+void Grafo::mostrar_coeficiente(){
+    int i = 1, qtd_amigos, busca, aux;
+    float coeficiente_grafo, soma_real = 0, coeficiente_maximo, coeficiente_real = 0;
+    _List_const_iterator <int> ponteiro, auxiliar;
+    while(i<=62){
+        ponteiro           = vizinhos[i].begin();
+        auxiliar           = ponteiro;
+        qtd_amigos         = vizinhos[i].size();//Quantos vizinhos cada golfinho tem
+        coeficiente_maximo = (qtd_amigos*(qtd_amigos-1))/2;
+        aux = qtd_amigos;
+        for(int j = 0; j < qtd_amigos; j++){
+            for(int k = 0; k < aux-1; k++){
+                auxiliar++;
+                //cout << "Ponteiro = " << *ponteiro << endl;
+                //cout << "Testando se tem " << *auxiliar << " no ponteiro" << endl;
+                if(find(vizinhos[*ponteiro].begin(), vizinhos[*ponteiro].end(),*auxiliar) != vizinhos[*ponteiro].end()){
+                    coeficiente_real++;
+                    //cout << "Coeficiente real aumentado, agora está em " << coeficiente_real << endl;
+                }
+            }
+            aux--;
+            ponteiro++;
+            auxiliar = ponteiro;
+        }
+        cout << "Coeficiente de aglomeracao do golfinho " << i << " = " << coeficiente_real << "/" << coeficiente_maximo << endl;
+        if(coeficiente_maximo != 0)
+            soma_real       += coeficiente_real/coeficiente_maximo;
+        coeficiente_real = 0;
+        i++; 
+    }
+    coeficiente_grafo = soma_real/62;
+    cout << "Coeficiente medio do grafo = " << coeficiente_grafo << endl; 
 }
 
 void ler_arquivo(Grafo &grafo){
@@ -69,10 +97,10 @@ void ler_arquivo(Grafo &grafo){
     }
 }
 
-int main()
-{
+int main(){
     Grafo grafo(62);
     ler_arquivo(grafo);
     grafo.mostrar_graus();
+    grafo.mostrar_coeficiente();
     return 0;
 }
